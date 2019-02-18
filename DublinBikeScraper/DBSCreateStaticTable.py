16c75f2d1967beca81eb02ae13b1a8da5a9ef72b
John McLoughlin <john.mc-loughlin.3@ucdconnect.ie>
2019-02-18 20:42:58 +0000

    Now with comments Also csv files are now empty
    Signed-off-by: John McLoughlin <john.mc-loughlin.3@ucdconnect.ie>

--- DublinBikeScraper/DBSCreateStaticTable.py
+++ DublinBikeScraper/DBSCreateStaticTable.py
@@ -5,20 +5,28 @@ from json import loads
 import sqlalchemy as sql
 import csv
 
+# URL for scraping
 url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=64f0cdd31dc5e915c6e192810317fb7971958fd3"
 
+# Convert json to python object (list of dictionaries)
 apiInfo = loads(get(url).text)
 
+# Create SQL engine
+# String:"mysql+pymysql://username:password@computer/database"
 engine = sql.create_engine("mysql+pymysql://root:Jm30079!@localhost/test")
 
+# Connect to SQL
 connection = engine.connect()
-"""
+
+# Iterate over api_info, build SQL insert
 for i in apiInfo:
     insert = 'INSERT INTO DbStaticInfo VALUES (%d,"%s","%s",%.6f,%.6f)' \
              % (i["number"],i["name"],i["address"],i["position"]["lat"],i["position"]["lng"])
 
+    # Execute SQL command
     connection.execute(insert)
-"""
+
+# Back up to csv file
 with open("DbStaticInfoBackUp.csv", "a") as csv_file:
     writer = csv.writer(csv_file)
     writer = csv.writer(csv_file)
@@ -28,4 +36,5 @@ with open("DbStaticInfoBackUp.csv", "a") as csv_file:
         print(writeRow)
         writer.writerow(writeRow)
 
+# Close sql connection
 connection.close()
--- DublinBikeScraper/DBScraper.py
+++ DublinBikeScraper/DBScraper.py
@@ -7,28 +7,41 @@ import csv
 from time import sleep
 import traceback
 
+
+# URL for scraping
 url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=64f0cdd31dc5e915c6e192810317fb7971958fd3"
 
+# Create SQL engine
+# String:"mysql+pymysql://username:password@computer/database"
 engine = sql.create_engine("mysql+pymysql://root:Jm30079!@localhost/test")
 
-
+# Method to write to sql database
 def writesql():
+    """
+    Writes rows to SQL table
+    Takes no input, output is new rows in SQL table
+    """
 
-
+    # Convert json to python object (list of dictionaries)
     api_info = loads(get(url).text)
 
+    # Connect to SQL
     connection = engine.connect()
 
+    # Iterate over api_info, build SQL insert
     for i in api_info:
         insert = 'INSERT INTO DbDynamicInfo (number, status, bikestands, available_bike_stands, available_bikes, last_update) ' \
                  'VALUES (%d,"%s",%d,%d,%d,%d)' \
                  % (i["number"],i["status"],i["bike_stands"],i["available_bike_stands"],
                     i["available_bikes"],i["last_update"])
 
+        # Execute SQL command
         connection.execute(insert)
 
+    # Close sql connection
     connection.close()
 
+    # Back up to csv file
     with open("DbDynamicInfoBackUp.csv", "a") as csv_file:
         writer = csv.writer(csv_file)
         writer = csv.writer(csv_file)
@@ -37,8 +50,9 @@ def writesql():
                     i["available_bikes"],i["last_update"]]
             writer.writerow(writeRow)
 
-
+# Main loop
 while True:
+    # try and except in case api down
     try:
         writesql()
         sleep(5*60)

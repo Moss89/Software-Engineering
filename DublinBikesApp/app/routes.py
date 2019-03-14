4d88207c739f6aca7220390f45f0aeb1562fd50e
john <john.hackett1@ucdconnect.ie>
2019-03-14 19:29:17 +0000

    Bikes now updating current availability
    Pop up above bikes now gives number of bikestands and number of available bikes

--- DublinBikesApp/app/routes.py
+++ DublinBikesApp/app/routes.py
@@ -3,6 +3,8 @@ from app import app
 from app.models import DbStaticInfo, DbDynamicInfo
 import helpers
 import requests
+from sqlalchemy.sql.expression import func
+from sqlalchemy.sql import select
 
 """
 Create the different routes here
@@ -12,18 +14,34 @@ Create the different routes here
 @app.route("/index", methods=['POST'])
 def index():
     try:
-        static_info = helpers.get_static_data(DbStaticInfo.query.all())
-        return render_template("index.html", len=len(static_info["lat"]),
-                               lat=static_info["lat"],
-                               lng=static_info["lng"],
-                               address=static_info["address"])
+        static_table = DbStaticInfo.query.all()
+        static_info = helpers.get_static_data(static_table)
+        max_value = select([func.max(DbDynamicInfo.last_update)])
+        dynamic_table = DbDynamicInfo.query.join(DbStaticInfo, (DbDynamicInfo.number == DbStaticInfo.number)).filter(DbDynamicInfo.last_update == max_value).all()
+        dynamic_info = helpers.get_dynamic_data(static_table, dynamic_table)
+        lat, lng, address, bikestands, available_bikes = ([] for i in range(5))
+        lat = static_info['lat']
+        lng = static_info['lng']
+        address = static_info['address']
+        bikestands = static_info['bikestands']
+        available_bikes = dynamic_info['available_bikes']
+        return render_template("index.html",
+                               len = len(lat),
+                               lat = lat,
+                               lng = lng,
+                               address = address,
+                               bikestands = bikestands,
+                               available_bikes = available_bikes)
     except:
-        return "Error: unable to fetch data"
+        return "Error: unable to fetch data."
 
 
 @app.route("/get_bike_info", methods=["POST"])
 def get_bike_info():
-    date_time = helpers.get_date_time().get_data(as_text=True)
-    query = (str(date_time[2:12]) + " " + str(date_time[15:20]))
-    available_bikes = helpers.get_dynamic_data(DbStaticInfo.query.join(DbDynamicInfo, (DbDynamicInfo.number == DbStaticInfo.number)).filter(DbDynamicInfo.last_update >= query), DbDynamicInfo.query.filter(DbDynamicInfo.last_update >= query))
-    return str(available_bikes)
+    try:
+        date_time = helpers.get_date_time().get_data(as_text=True)
+        query = (str(date_time[2:12]) + " " + str(date_time[15:20]))
+        available_bikes = helpers.get_dynamic_data(DbStaticInfo.query.join(DbDynamicInfo, (DbDynamicInfo.number == DbStaticInfo.number)).filter(DbDynamicInfo.last_update >= query), DbDynamicInfo.query.filter(DbDynamicInfo.last_update >= query))
+        return str(available_bikes)
+    except:
+        return "Error: unable to fetch dynamic data."
--- DublinBikesApp/helpers.py
+++ DublinBikesApp/helpers.py
@@ -6,25 +6,25 @@ Functions for the routes go here
 
 def get_static_data(table):
 
-    results = {"lat": [], "lng": [], "address": []}
+    results = {"lat": [], "lng": [], "address": [], "bikestands": []}
     for i in range(len(table)):
         results["lat"].append(table[i].lat)
         results["lng"].append(table[i].lng)
         results["address"].append(table[i].address)
+        results["bikestands"].append(table[i].bikestands)
     return results
 
 
 def get_dynamic_data(static_table, dynamic_table):
 
-    results = {"available_bikes": [], "address": []}
+    dynamic_results = {"available_bikes": [], "address": []}
     for i in range(0,113):
-        results["address"].append(static_table[i].address)
-        results["available_bikes"].append(dynamic_table[i].available_bikes)
-    return results
+        dynamic_results["address"].append(static_table[i].address)
+        dynamic_results["available_bikes"].append(dynamic_table[i].available_bikes)
+    return dynamic_results
 
 
 def get_date_time():
-
-    date =  request.form['date'];
+    date = request.form['date'];
     time = request.form['time'];
     return jsonify(date, time)
